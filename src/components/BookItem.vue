942806c2f9f25cde7fb41614c21eab72ff66b1c1
Shin Thomas (CI/DAD4.4) <thomas.shin@us.bosch.com>
2019-08-19 16:48:50 -0500

    BookItem

---
+++ src/components/BookItem.vue
@@ -0,0 +1,18 @@
+<template>
+<ul>
+    <li>{{book.title}}:{{book.author}}</li>
+</ul>
+</template>
+<script>
+export default {
+    name: BookItem,
+    props: 'book',
+}
+</script>
+
+<style>
+li {
+    display: block;
+    margin: 0 10px;
+}
+</style>
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -31,8 +31,5 @@ ul {
     list-style-type: none;
     padding: 0;
 }
-li {
-    display: block;
-    margin: 0 10px;
-}
+
 </style>
